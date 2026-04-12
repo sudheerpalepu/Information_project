7282706b9cb9a71a0f537885ae2baeb571b144f0
sudheerpalepu <146517155+sudheerpalepu@users.noreply.github.com>
2026-04-12 15:00:37 +0100

    use environment variable for database

--- backend/db.py
+++ backend/db.py
@@ -1,21 +1,18 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# Read from Render environment variable
 DATABASE_URL = os.getenv("postgresql://job_dashboard_db_user:ykbUEwpkoRbkqaK78EptHzaIKRgK2iQq@dpg-d7dq0n3bc2fs73e5nq50-a.oregon-postgres.render.com/job_dashboard_db")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL is not set in environment variables")
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
